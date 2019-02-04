6d8324c16a2cb848d25dff309ef49d83bab9dd54
aalisveris04 <aalisveris04@gmail.com>
2019-02-03 19:58:19 -0800

    Renamed  OtherHatchMech into ExpanderHatch

---
+++ src/main/java/com/teamcautionrobotics/robot/ExpanderHatch.java
@@ -0,0 +1,26 @@
+
+package com.teamcautionrobotics.robot;
+
+import edu.wpi.first.wpilibj.*;
+
+public class ExpanderHatch{
+    
+    // pneumatics object push out
+    private Solenoid extender;
+    // pneumatics object pull back
+    private Solenoid expander;
+
+    public OtherHatch(int extenderPort, int expanderPort) {
+        extender = new Solenoid(extenderPort);
+        expander = new Solenoid(expanderPort);
+    }
+
+    public void deploy(boolean extend) {
+        extender.set(extend);
+
+    }
+    public void pullback(boolean out) {
+        expander.set(out);
+    }
+
+}
