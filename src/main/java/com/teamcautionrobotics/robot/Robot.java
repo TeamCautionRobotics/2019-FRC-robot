c868660c0a8c15c02d9b21744dc9c99abda45f40
Sean <seantierney42@gmail.com>
2019-02-06 18:00:41 -0700

    Delete unnecessary	autonomous filler code (auto-generated).

--- src/main/java/com/teamcautionrobotics/robot/Robot.java
+++ src/main/java/com/teamcautionrobotics/robot/Robot.java
@@ -38,11 +38,6 @@ public class Robot extends TimedRobot {
 
     boolean deployButtonPressed = false;
 
-    private static final String kDefaultAuto = "Default";
-    private static final String kCustomAuto = "My Auto";
-    private String m_autoSelected;
-    private final SendableChooser<String> m_chooser = new SendableChooser<>();
-
     /**
      * This function is run when the robot is first started up and should be used
      * for any initialization code.
@@ -56,10 +51,6 @@ public class Robot extends TimedRobot {
         driveBase = new DriveBase(0, 1, 0, 1, 2, 3);
 
         timer = new Timer();
-
-        m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-        m_chooser.addOption("My Auto", kCustomAuto);
-        SmartDashboard.putData("Auto choices", m_chooser);
     }
 
     /**
@@ -101,23 +92,8 @@ public class Robot extends TimedRobot {
         driveBase.drive(driveLeftCommand, driveRightCommand);
     }
 
-    /**
-     * This autonomous (along with the chooser code above) shows how to select
-     * between different autonomous modes using the dashboard. The sendable chooser
-     * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
-     * remove all of the chooser code and uncomment the getString line to get the
-     * auto name from the text box below the Gyro
-     *
-     * <p>
-     * You can add additional auto modes by adding additional comparisons to the
-     * switch structure below with additional strings. If using the SendableChooser
-     * make sure to add them to the chooser code above as well.
-     */
     @Override
     public void autonomousInit() {
-        m_autoSelected = m_chooser.getSelected();
-        // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-        System.out.println("Auto selected: " + m_autoSelected);
     }
 
     /**
@@ -125,15 +101,6 @@ public class Robot extends TimedRobot {
      */
     @Override
     public void autonomousPeriodic() {
-        switch (m_autoSelected) {
-        case kCustomAuto:
-            // Put custom auto code here
-            break;
-        case kDefaultAuto:
-        default:
-            // Put default auto code here
-            break;
-        }
     }
 
     /**
