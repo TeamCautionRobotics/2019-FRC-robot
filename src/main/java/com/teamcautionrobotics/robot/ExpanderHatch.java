129b72c894604a443ed6c313e576da5743fa164f
Sean <seantierney42@gmail.com>
2019-02-13 20:41:21 -0700

    Formatting for ExpanderHatch, make fields final.

--- src/main/java/com/teamcautionrobotics/robot/ExpanderHatch.java
+++ src/main/java/com/teamcautionrobotics/robot/ExpanderHatch.java
@@ -6,9 +6,9 @@ import edu.wpi.first.wpilibj.*;
 public class ExpanderHatch{
     
     // pneumatics object push out
-    private Solenoid extender;
+    private final Solenoid extender;
     // pneumatics object pull back
-    private Solenoid expander;
+    private final Solenoid expander;
 
     public ExpanderHatch(int extenderPort, int expanderPort) {
         extender = new Solenoid(extenderPort);
@@ -17,10 +17,8 @@ public class ExpanderHatch{
 
     public void deploy(boolean extend) {
         extender.set(extend);
-
     }
     public void pullback(boolean out) {
         expander.set(out);
     }
-
 }
