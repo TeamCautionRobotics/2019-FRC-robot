d3cb0a79e20e62396b1c5c040fdbce2301fd46ac
Sean <seantierney42@gmail.com>
2019-02-13 19:29:36 -0700

    Change to only using one motor.

--- src/main/java/com/teamcautionrobotics/robot/Cargo.java
+++ src/main/java/com/teamcautionrobotics/robot/Cargo.java
@@ -5,8 +5,7 @@ import edu.wpi.first.wpilibj.*;
 
 public class Cargo {
     //motor object
-    VictorSP funnelRoller;
-    Victor backRollerConveyor;
+    private final VictorSP funnelRoller;
     //pneumatics object
     Solenoid deployFunnelRoller;
     Solenoid deployExitFlap;
@@ -15,28 +14,13 @@ public class Cargo {
 
     public Cargo(int funnelRollerPort, int backRollerConveyorPort, int deployFunnelRollerPort, int deployExitFlapPort) {
         funnelRoller = new VictorSP(funnelRollerPort);
-        backRollerConveyor = new VictorSP(backRollerConveyorPort);
         deployFunnelRoller = new Solenoid(deployFunnelRollerPort);
         deployExitFlap = new Solenoid(deployExitFlapPort);
     }
 
-    public void intake(boolean intake, boolean spitout) {
-        if(intake){
-            funnelRoller.set(127);
-            if(deployedFunnelRoller){
-                backRollerConveyor.set(127);
-            }
-        }
-        else if(spitout){
-            funnelRoller.set(-127);
-                if(deployedFunnelRoller){
-                backRollerConveyor.set(-127);
-            }        }
-        else{
-            funnelRoller.set(0);
-                if(deployedFunnelRoller){
-                backRollerConveyor.set(0);
-            }        }
+    public void intake(double power) {
+        funnelRoller.set(power);
+    }
 
     }
 
