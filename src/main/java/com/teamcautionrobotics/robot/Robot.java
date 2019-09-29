e9adf79b89a7580d216392589af8d49f5aeefc76
Sean <seantierney42@gmail.com>
2019-09-29 15:11:25 -0700

    Map the grabber toggle to right joystick buttons
    Buttons 2, 3, 4, and 5 all have this functionality.

--- src/main/java/com/teamcautionrobotics/robot/Robot.java
+++ src/main/java/com/teamcautionrobotics/robot/Robot.java
@@ -37,16 +37,16 @@ public class Robot extends TimedRobot {
      * 
      * Driver controls:
      * 
-     * Left joystick: X axis, robot turn control; Button 1, Jack for HAB;
-     * Button 2, Toggle aiming lights
+     * Left joystick: X axis, robot turn control; Button 1, Jack for HAB; Button 2,
+     * Toggle aiming lights
      * 
      * Right Joystick: Y axis, robot forward and backward control; Button 2, smooth
      * deriving toggle; Button 3, precision turning mode
      * 
      * Gamepad: Left thumbstick, Rotate hatch arm; A, Deploy funnel roller (cargo
-     * mechanism extender); B, Deploy hatch (velcro mech); X, Cargo deploy exit flap; Right
-     * trigger, Cargo; Left trigger, Cargo reverse; Right bumper, Expand expander
-     * hatch mech; Left bumper, Extend expander hatch mech past bumper zone
+     * mechanism extender); B, Deploy hatch (velcro mech); X, Cargo deploy exit
+     * flap; Right trigger, Cargo; Left trigger, Cargo reverse; Right bumper, Expand
+     * expander hatch mech; Left bumper, Extend expander hatch mech past bumper zone
      *
      * All pneumatics are toggles except for the velcro hatch deployer and the cargo
      * exit flap.
@@ -134,7 +134,8 @@ public class Robot extends TimedRobot {
         if (!USING_VELCRO_HATCH) {
             reacherToggleRunner = new ButtonToggleRunner(() -> manipulator.getButton(Button.LEFT_BUMPER),
                     expanderHatch::toggleReacher);
-            grabberToggleRunner = new ButtonToggleRunner(() -> manipulator.getButton(Button.RIGHT_BUMPER),
+            grabberToggleRunner = new ButtonToggleRunner(() -> (driverRight.getRawButton(2)
+                    || driverRight.getRawButton(3) || driverRight.getRawButton(4) || driverRight.getRawButton(5)),
                     expanderHatch::toggleGrabber);
         }
 
